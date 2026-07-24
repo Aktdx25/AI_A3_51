9eaac1120269d24e8b05761d4544644888e652b1
Aktdx25 <aamin.khan25@mmit.edu.in>
2026-07-24 11:50:53 +0530

    Added a factorial calculation file

---
+++ Artificial_Intelligence_Lab/Factorial_program.py
@@ -0,0 +1,20 @@
+def f(a):
+ fact=1
+ while(a!=1):
+  fact=fact*(a)
+  a=a-1
+ print(f"Factorial of the given number = ",fact)
+ return fact
+
+
+a=float(input("Enter the number you want to find factorial of: "))
+print(f"The number whose factorial is to be found : ",a)
+
+if(a<0):
+   print("Factorial does not exist for negative number: ")
+elif (a==0 or a==1):
+  print("Factorial = 1")  
+else:
+  b=f(a)
+
+
